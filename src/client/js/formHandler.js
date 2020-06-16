1d3075c4983368e449da20c05d1d4d79f3f61ead
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-06-16 19:55:26 -0400

    Make Changes to formHandler and Server

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -12,6 +12,22 @@ function handleSubmit(event) {
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
     })
+
+    let urlSubmission = {
+        url: formText
+    };
+
+    console.log("::: Form Submitted :::")
+    // TODO: Fetch API with localhost target
+    fetch('http://localhost:8081/aylien', {
+        method: 'POST',
+        body: JSON.stringify(urlSubmission),
+        headers: {"Content-Type": "application/json"}
+    })
+    .then(res => res.json())
+    .then(function(res) {
+        document.getElementById('results').innerHTML = JSON.stringify(res);
+    })
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -11,23 +11,15 @@ var AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
   application_id: process.env.API_ID,
   application_key: process.env.API_KEY,
-  url: "https://api.aylien.com/api/v1/sentiment"
 });
-
-textapi.sentiment({
-    'text': 'John is a very good football player!'
-  }, function(error, response) {
-    if (error === null) {
-      console.log(response);
-    }
-  });
 
 const app = express();
 
+app.use(cors());
 app.use(express.static('dist'));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 console.log(__dirname);
 
@@ -43,3 +35,15 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse);
 })
+
+app.post('/aylien', (request, response) => {
+    let articleURL = request.body.url;
+    console.log(articleURL);
+    textapi.sentiment({
+      'text': 'John is a very good football player!'
+    }, function(error, response) {
+      if (error === null) {
+        console.log(response);
+      }
+    });
+});
