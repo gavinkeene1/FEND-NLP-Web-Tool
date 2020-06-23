848f089ebcb16ab64794a94900a68e56ff9966f4
Gavin Keene <64376895+gavinkeene1@users.noreply.github.com>
2020-06-22 21:36:45 -0400

    Update Validation Test For Greater >140 Characters

--- src/client/js/nameChecker.test.js
+++ src/client/js/nameChecker.test.js
@@ -15,7 +15,8 @@ test('Verify that form input validation is <= 140 characters', () => {
 test('Verify that form input validation rejects > 140 characters', () => {
     // Start with a base 140-character phrase
     let testPhrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales. Nullam mattis fermentum libero, non volutpat.";
-    for (let index = 140; index >= 140; index += 200) {
-        testPhrase += "abcdefghijklmnopqrstuvwxyz"
+    for (let index = 0; index < 100; index ++) {
+        testPhrase += "abcdefghijklmnopqrstuvwxyz";
     }
+    expect(inputLength(testPhrase)).toBe(false);
 });
